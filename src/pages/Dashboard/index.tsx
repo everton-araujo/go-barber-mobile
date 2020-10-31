b011ccb49398208e87e8f6d252888dd7c0724ad6
Everton Araujo <everton.aaraujo@outlook.com>
2020-10-31 12:13:00 -0300

    Listing providers from api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,40 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
-import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar } from './styles';
+import {
+    Container,
+    Header,
+    HeaderTitle,
+    UserName,
+    ProfileButton,
+    UserAvatar,
+    ProvidersList
+} from './styles';
+
+export interface Provider {
+    id: string;
+    name: string;
+    avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
+    const [providers, setProviders] = useState<Provider[]>([]);
+
     const { signOut, user } = useAuth();
     const { navigate } = useNavigation();
 
+    useEffect(() => {
+        api.get('providers').then(response => {
+            setProviders(response.data);
+        });
+    }, []);
+
     const navigateToProfile = useCallback(() => {
-        navigate('Profile')
+        navigate('Profile');
+        signOut();
     }, [navigate]);
 
     return (
@@ -25,6 +49,13 @@ const Dashboard: React.FC = () => {
                     <UserAvatar source={{ uri: user.avatar_url }} />
                 </ProfileButton>
             </Header>
+
+            <ProvidersList
+                data={providers}
+                keyExtractor={(provider) => provider.id}
+                renderItem={({ item }) => <UserName>{item.name}</UserName>}
+            />
+
         </Container>
     );
 };
