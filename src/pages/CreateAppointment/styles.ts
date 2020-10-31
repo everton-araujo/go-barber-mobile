4aa1b2ef7b020139a77faecf7c95418a9a7e8b0f
Everton Araujo <everton.aaraujo@outlook.com>
2020-10-31 15:45:46 -0300

    User can chose provider on createAppointment

---
+++ src/pages/CreateAppointment/styles.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
+import { FlatList } from 'react-native';
+import { Provider } from './index';
+
+interface ProviderContainerProps {
+    selected: boolean;
+}
+
+interface ProviderNameProps {
+    selected: boolean;
+}
+
+export const Container = styled.View`
+    flex: 1;
+`;
+
+export const Header = styled.View`
+   padding: 24px;
+    /* padding-top: ${getStatusBarHeight() + 24}px; */
+    background: #28262E;
+
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+
+`;
+
+export const HeaderTitle = styled.Text`
+    color: #F4EDE8;
+    font-family: 'RobotoSlab-Mediu';
+    font-size: 20px;
+    margin-left: 16px;
+`;
+
+export const UserAvatar = styled.Image`
+    width: 56px;
+    height: 56px;
+    border-radius: 28px;
+    margin-left: auto;
+`;
+
+export const ProvidersListContainer = styled.View`
+    height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+    padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton) <ProviderContainerProps>`
+    background: ${(props) => (props.selected ? '#FF9000' : '#3E3B47')};
+    flex-direction: row;
+    align-items: center;
+    padding: 8px 12px;
+    margin-right: 16px;
+    border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+    width: 32px;
+    height: 32px;
+    border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+    margin-left: 8px;
+    font-family: 'RobotoSlab-Mediu';
+    font-size: 16px;
+    color: ${(props) => (props.selected ? '#232129' : '#F4EDE8')};
+`;
