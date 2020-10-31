4a8a3723672f3ff6b95cfb759e3e64372b38ae11
Everton Araujo <everton.aaraujo@outlook.com>
2020-10-31 12:56:27 -0300

    Card created for each provider on the list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
@@ -11,7 +12,14 @@ import {
     UserName,
     ProfileButton,
     UserAvatar,
-    ProvidersList
+    ProvidersListTitle,
+    ProvidersList,
+    ProviderContainer,
+    ProviderAvatar,
+    ProviderInfo,
+    ProviderName,
+    ProviderMeta,
+    ProviderMetaText
 } from './styles';
 
 export interface Provider {
@@ -33,8 +41,12 @@ const Dashboard: React.FC = () => {
     }, []);
 
     const navigateToProfile = useCallback(() => {
-        navigate('Profile');
+        // navigate('Profile');
         signOut();
+    }, [navigate]);
+
+    const navigateToCreateAppointment = useCallback((providerId: string) => {
+        navigate('CreateAppointment', { providerId });
     }, [navigate]);
 
     return (
@@ -53,7 +65,28 @@ const Dashboard: React.FC = () => {
             <ProvidersList
                 data={providers}
                 keyExtractor={(provider) => provider.id}
-                renderItem={({ item }) => <UserName>{item.name}</UserName>}
+                ListHeaderComponent={
+                    <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+                }
+                renderItem={({ item: provider }) => (
+                    <ProviderContainer onPress={() => navigateToCreateAppointment(provider.id)}>
+                        <ProviderAvatar source={{ uri: provider.avatar_url }} />
+
+                        <ProviderInfo>
+                            <ProviderName>{provider.name}</ProviderName>
+
+                            <ProviderMeta>
+                                <Icon name="calendar" size={14} color="#FF9000" />
+                                <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+                            </ProviderMeta>
+
+                            <ProviderMeta>
+                                <Icon name="clock" size={14} color="#FF9000" />
+                                <ProviderMetaText>8h às 18h</ProviderMetaText>
+                            </ProviderMeta>
+                        </ProviderInfo>
+                    </ProviderContainer>
+                )}
             />
 
         </Container>
