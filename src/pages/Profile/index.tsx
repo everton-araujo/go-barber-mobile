80faa318fab12c1a847eede7cd54d493b7713b69
Everton Araujo <everton.aaraujo@outlook.com>
2020-10-31 23:27:48 -0300

    Profile avatar update

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -12,6 +12,7 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
+import ImagePicker from 'react-native-image-picker';
 
 import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -109,11 +110,39 @@ const SignUp: React.FC = () => {
                 'Ocorreu um erro ao atualizar seu perfil, tente novamente'
             );
         }
-    }, [navigation]);
+    }, [navigation, updateUser]);
 
     const formSubmit = () => {
         formRef.current?.submitForm()
     };
+
+    const handleUpdateAvatar = useCallback(() => {
+        ImagePicker.showImagePicker({
+            title: 'Selecione um avatar',
+            cancelButtonTitle: 'Cancelar',
+            takePhotoButtonTitle: 'User câmera',
+            chooseFromLibraryButtonTitle: 'Escolher da galeria'
+        }, response => {
+            if (response.didCancel) {
+                return;
+            } else if (response.error) {
+                Alert.alert('Erro ao atualizar seu avatar');
+                return;
+            }
+
+            const data = new FormData();
+
+            data.append('avatar', {
+                type: 'image/jpg',
+                name: `${user.id}.jpg`,
+                uri: response.uri,
+            });
+
+            api.patch('users/avatar', data).then(apiResponse => {
+                updateUser(apiResponse.data);
+            });
+        });
+    }, [updateUser, user.id]);
 
     const handleGoBack = useCallback(() => {
         navigation.goBack();
@@ -135,7 +164,7 @@ const SignUp: React.FC = () => {
                             <Icon name="chevron-left" size={24} color="#999591" />
                         </BackButton>
 
-                        <UserAvatarButton onPress={() => { }}>
+                        <UserAvatarButton onPress={handleUpdateAvatar}>
                             <UserAvatar source={{ uri: user.avatar_url }} />
                         </UserAvatarButton>
 
